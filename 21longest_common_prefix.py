c791f312d8c9b878df34e7ec386740ad6e9e8581
coding sigma <92455602+GaneshRenuse@users.noreply.github.com>
2023-09-08 20:14:47 +0530

    Update

--- 21longest_common_prefix.py
+++ 21longest_common_prefix.py
@@ -1,3 +1,5 @@
+# Solution : In python 3
+
 class Solution:
     def longestCommonPrefix(self, v: List[str]) -> str:
         ans=""
@@ -9,7 +11,9 @@ class Solution:
                 return ans
             ans+=f[i]
         return ans
-
+        
+# driver code for running 
+"""
 s = Solution()
 
 plist = ["flower", "flow", "flight"]
@@ -17,3 +21,13 @@ plist = ["flower", "flow", "flight"]
 outputlist = s.longestCommonPrefix(plist)
 
 print(outputlist)
+"""
+# 
+
+# Explanation : line by line
+# The code starts by creating a list that will store all the characters in both strings.
+# Then, it sorts them and stores the first character in one string as 0 and last character in another string as -1.
+# It then iterates through each index of both lists until it finds a mismatch between the two lists (a difference between their first or last characters).
+# The code then returns an empty string if there is no match found, otherwise it adds up all the characters from its own list and appends those to itself.
+# The code will return the longest common prefix of the input list.
+
